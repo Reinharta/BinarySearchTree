acae5d46b36b662f4ff8f59eebb8dde3663dae3f
Anina Reinhart <reinharta@outlook.com>
2018-06-20 20:20:49 -0500

    Node class created, Tree class started, Add() started

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryTree
+    {
+        private Node root;
+
+
+        public Node Root { get => root; } 
+
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+        public BinaryTree(int initialValue)
+        {
+            root = new Node(initialValue);
+        }
+
+
+        public void Add(int newValue)
+        {
+            AddToTree(root, newValue);
+        }
+        private void AddToTree(Node currentNode, int newValue)
+        {
+            if(currentNode == null)
+            {
+                Node newNode = new Node(newValue);
+                currentNode = newNode;
+                return;
+            }
+            if (newValue < currentNode.Value)
+            {
+                AddToTree(currentNode.LeftChild, newValue);
+                return;
+            }
+            if (newValue > currentNode.Value)
+            {
+                AddToTree(currentNode.RightChild, newValue);
+                return;
+            }
+            if (newValue == currentNode.Value)
+            {
+
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        private int value;
+        private Node parent;
+        private Node leftChild;
+        private Node rightChild;
+
+        public int Value { get => value; set => this.value = value; }
+        public Node Parent { get => parent; set => parent = value; }
+        public Node LeftChild { get => leftChild; set => leftChild = value; }
+        public Node RightChild { get => rightChild; set => rightChild = value; }
+
+        public Node(int value)
+        {           
+                this.value = value;
+                parent = null;
+                leftChild = null;
+                rightChild = null;
+        }
+
+        }
+            
+
+
+
+}
